e0fef6059b8062c51bbd1c6745d8776d127e7fd6
Paola Cespedes <any.cespedes8@gmail.com>
2019-03-30 17:51:48 -0400

    Añadir  la clase cajas

---
+++ Cajas.java
@@ -0,0 +1,34 @@
+/**
+ * @author Paola Cespedes
+ */
+public class Cajas {
+
+    Integer largo;
+    Integer ancho;
+    Integer posicion [][];
+
+
+    public Integer getLargo() {C:\Users\MM\Desktop\UMMS\CURSOS\Full-Stack\Workshops\git&github\test\sokoban-A
+        return largo;
+    }
+
+    public void setLargo(Integer largo) {
+        this.largo = largo;
+    }
+
+    public Integer getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(Integer ancho) {
+        this.ancho = ancho;
+    }
+
+    public Integer[][] getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Integer[][] posicion) {
+        this.posicion = posicion;
+    }
+}
